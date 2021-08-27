4b56138456a084c0f6ba6d55284891539e02b950
lucianaap <lucianaarmandpilon@outlook.com>
2021-08-26 23:35:31 -0300

    update

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,26 +1,29 @@
-const products_url = "https://lucianaap.github.io/proyecto_alternativo_JaP/productos.json"
+const products_url = "https://lucianaap.github.io/proyecto_alternativo_JaP/assets/json/productos.json"
 
 var getJSONData = function(url){
-    var result = {};
-    showSpinner();
-    return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }else{
-        throw Error(response.statusText);
-      }
-    })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
-    })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
+  var result = {};
+  showSpinner();
+  return fetch(url)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }else{
+      throw Error(response.statusText);
+    }
+  })
+  .then(function(response) {
+        result.status = 'ok';
+        result.data = response;
         hideSpinner();
         return result;
-    });
+  })
+  .catch(function(error) {
+      result.status = 'error';
+      result.data = error;
+      hideSpinner();
+      return result;
+  });
 }
+
+document.addEventListener("DOMContentLoaded", function(e){
+});
--- assets/js/productos.js
+++ assets/js/productos.js
@@ -5,7 +5,7 @@ function showProductsList(productsArray) {
         
         let donas = productsArray[i];
         list += `<h4 id="donas-name">` + donas.nombre + `</h4>`+ "<br>";
-        list +=  donas.descripcion + "<br>";3
+        list +=  donas.descripcion + "<br>";
         list += donas.precio + "<br>";
         list += "<br>" +  `<img id="imagen-producto" src=" `+ donas.imgSrc + `"alt = `+ donas.descripcion + `` + "<br>"
         list += "<br><hr><br>"
